8a0b62ab9db9f01ee8355acf0f3afe98a20ea1af
joshliford <j_liford1@yahoo.com>
2025-12-10 19:51:20 -0600

    completed conditional and loop exercises

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,17 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        JavaCafe javaCafe = new JavaCafe();
-        javaCafe.run();
+        //JavaCafe javaCafe = new JavaCafe();
+        //javaCafe.run();
+        ConditionalExercises conditional = new ConditionalExercises();
+        LoopExercises loops = new LoopExercises();
+
+
+        System.out.println(conditional.lessThanFive(2));
+        System.out.println(conditional.getAgeGroup(20));
+        System.out.println(conditional.isValidPassword("pass"));
+
+        System.out.println(loops.sum(4));
+        System.out.println(loops.sumUntilEven(3));
     }
 }
